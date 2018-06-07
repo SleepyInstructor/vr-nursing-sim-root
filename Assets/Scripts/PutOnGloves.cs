bbec07e99b0b83e7acbfeaf67ff6459c6bc389ab
erin <erin@langara.ca>
2018-06-07 10:55:55 -0800

    Working on glove textures

---
+++ Assets/Scripts/PutOnGloves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutOnGloves : MonoBehaviour {
+    private Texture latex;
+    private Renderer render;
+
+	// Use this for initialization
+	private void Start () {
+        render = GetComponent<Renderer>();
+        latex = Resources.Load("latex") as Texture;
+	}
+
+    /** OnTriggerEnter
+     *  Changes the material of the hands to latex when coming into 
+     *  contact with the box of gloves, signifying puttign on and wearing gloves
+     **/
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "gloves" && render.materials[1].mainTexture != latex)
+            render.materials[1].mainTexture = latex;
+        // else back to hands? 
+    }
+
+}
