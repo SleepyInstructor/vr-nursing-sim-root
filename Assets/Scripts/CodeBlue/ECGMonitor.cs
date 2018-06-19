96b65249440069550a023d5d14ec70d97fabb79f
erin <erin@langara.ca>
2018-06-19 10:57:59 -0800

    Lots of new assets, TV plays heartrate monitor footage

---
+++ Assets/Scripts/CodeBlue/ECGMonitor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ECGMonitor : MonoBehaviour {
+    private VideoPlayer player;
+
+    private VideoClip normalReading;
+    private VideoClip cardiacArrest;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<VideoPlayer>();
+
+        normalReading = Resources.Load("ECG Monitor Loop") as VideoClip;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
