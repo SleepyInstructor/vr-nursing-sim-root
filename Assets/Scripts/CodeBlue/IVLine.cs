49239a0dbdeebaf0702c43e624035470b9133216
erin <erin@langara.ca>
2018-07-03 10:59:13 -0800

    Door animations for JobSim, IV tubing for CB

---
+++ Assets/Scripts/CodeBlue/IVLine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IVLine : MonoBehaviour {
+    public const int MaxObjects = 20;
+
+    private static int numObjects = 0;
+	// Use this for initialization
+	void Start () {
+        if (numObjects < MaxObjects) {
+            GameObject nextLink = Instantiate(this.gameObject, this.transform);
+            GetComponent<HingeJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+            numObjects++;
+        } else {
+            GetComponent<HingeJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+        }
+	}
+}
---
+++ Assets/Scripts/JobSim/OpenDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+    public GameObject door;
+
+    private Animator animator;
+
+    private void Start() {
+        animator = door.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("patientSideDoor")) {
+            animator.SetBool("PatientDoor", true);
+        } else if (other.CompareTag("storageSideDoor")) {
+            animator.SetBool("StorageDoor", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("patientSideDoor")) {
+            animator.SetBool("PatientDoor", false);
+        } else if (other.CompareTag("storageSideDoor")) {
+            animator.SetBool("StorageDoor", false);
+        }
+    }
+}
